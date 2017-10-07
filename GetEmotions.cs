532ea6f7097dccb1e1227851331c15377e4a6a43
Raghav Gupta <raghav0296@gmail.com>
2017-10-07 14:26:41 -0400

    Add files via upload
    added score

--- GetEmotions.cs
+++ GetEmotions.cs
@@ -7,6 +7,8 @@ using IBM.Watson.DeveloperCloud.Utilities;
 public class GetEmotions : MonoBehaviour {
     ToneAnalyzer _toneAnalyzer;
     public string _stringToTest;
+    public float result_score;
+    public bool analyzingFinish;
     // Use this for initialization
     void Start () {
         Credentials credentials = new Credentials("078652c5-dafe-4774-8fa5-d651d3ffb1f2", "lMouYHxZVPjx", "https://gateway.watsonplatform.net/tone-analyzer/api");
@@ -14,8 +16,12 @@ public class GetEmotions : MonoBehaviour {
         _toneAnalyzer = new ToneAnalyzer(credentials);
 
 
-        _stringToTest = "I am feeling sad.";
-        AnalyzeTone();
+        AnalyzeTone("this is sad.");
+        AnalyzeTone("we should be happy.");
+        AnalyzeTone("this should work now.");
+        AnalyzeTone("i dont feel good anymore.");
+        AnalyzeTone("this is not right.");
+        AnalyzeTone("it did not work out well.");
     }
 	
 	// Update is called once per frame
@@ -23,18 +29,38 @@ public class GetEmotions : MonoBehaviour {
 		
 	}
 
-    public void AnalyzeTone()
+    public void AnalyzeTone(string _stringToTest)
     {
-
         if (!_toneAnalyzer.GetToneAnalyze(OnGetToneAnalyze, _stringToTest))
             print("Watson Tone Analyzer: Failed to analyze");
-        else
-            print(_stringToTest);
-            print("working?");
+      
     }
 
     public void OnGetToneAnalyze(ToneAnalyzerResponse resp, string data)
     {
-        print(data);
+
+        float score_sadness = get_emotion_score(data, "sadness");
+        float score_analytical = get_emotion_score(data, "analytical");
+        float score_anger = get_emotion_score(data, "anger");
+        float score_confident = get_emotion_score(data, "confident");
+        float score_tentative = get_emotion_score(data, "tentative");
+        float score_fear = get_emotion_score(data, "fear");
+        float score_joy = get_emotion_score(data, "joy");
+
+        result_score = score_sadness * -1 + score_analytical * 1 + score_anger * -2 + score_confident * 2 + score_tentative * 1 + score_fear * -1 + score_joy * 2;
+
+    }
+
+    public float get_emotion_score(string data, string emotion)
+    {
+        float score = 0;
+        int index = data.IndexOf(emotion);
+        if (index != -1)
+        {
+            string tmp = data.Remove(index, data.Length-index);
+            tmp = tmp.Split(',')[tmp.Split(',').Length - 2];
+            score = float.Parse(tmp.Split(':')[tmp.Split(':').Length - 1]);
+        }
+        return score;
     }
 }
