e351959400bbc7ee7ae0b6678572ccdc22e05e13
Raghav Gupta <raghav0296@gmail.com>
2017-10-08 15:59:37 -0400

    Add files via upload

---
+++ Server Scripts/fbBotInterface.py
@@ -0,0 +1,41 @@
+import socket
+import wysabot
+import _thread
+import time
+
+wysa = wysabot.wysaBot()
+socket_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_connection.bind(('18.111.9.226', 4599))
+message = 'This is the message.'
+socket_connection.listen(1)
+client_connection, client_address = socket_connection.accept()
+
+
+def receive_from_wysa():
+    while True:
+        try:
+            time.sleep(1)
+            messages = wysa.retrieve_messages()
+            if len(messages)!=0:
+                messages = messages + "\n\r"
+                client_connection.send(messages.encode())
+        except:
+            client_connection.close()
+
+
+_thread.start_new_thread(receive_from_wysa, ())
+
+while True:
+    try:
+        print("Receiving from ", client_address)
+        data = client_connection.recv(1028)
+        if data:
+            data = data.decode().rstrip("\n\r")
+            print("to send "+data, ", length: ", len(data))
+            if len(data)!=0:
+                wysa.send_message(data)
+        else:
+            break
+
+    finally:
+        pass
---
+++ Server Scripts/wysabot.py
@@ -0,0 +1,46 @@
+from fbchat import Client
+from fbchat.models import *
+
+
+class wysaBot:
+    def __init__(self):
+        with open("mseal", "rb") as f:
+            mseal = f.read().decode()
+        f.close()
+        self.client = Client("raghav0296@gmail.com", password=mseal)
+        # self.client = Client("8573088029", password="triloka96")
+        self.thread_id = self.client.searchForPages("Wysa - happiness chatbot therapy")[0].uid
+        self.prev_msg = ""
+        self.prev_msg = self.retrieve_messages()
+
+    def send_message(self, message):
+        self.client.sendMessage(message, thread_id=self.thread_id, thread_type=ThreadType.USER)
+        self.prev_msg = message
+
+    def retrieve_messages(self):
+        all_messages = self.client.fetchThreadMessages(thread_id=self.thread_id, limit=10)
+        message_texts = []
+        for i in all_messages:
+            if i.text == self.prev_msg:
+                break
+            i.text = self.filter_messages(i.text)
+            message_texts.append(i.text)
+        self.prev_msg = all_messages[0].text
+        message_texts = str.join("", message_texts[::-1])
+        return message_texts
+
+    def filter_messages(self, message):
+        message = str(message)
+        filtered_message = ""
+        for m in message:
+            if m.isalnum() or m.isspace() or m in [",",".","'","-","_","?","!"]:
+                filtered_message+=m
+        message = filtered_message
+        message.replace("Raghav","")
+        message.replace("Please choose an option from below.","")
+        if "Here are some commands I understand" in message:
+            message = ""
+        if "Say #MOOD or #ANGRY" in message:
+            message = ""
+        return message
+
